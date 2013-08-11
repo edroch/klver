45f5a99e572ef5254c6910714ec367d1a18950f6
Ed Rocha <edrocha@gmail.com>
2013-08-11 14:45:51 -0400

    Setup updates

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'app_klver',
+			'database'  => 'database',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
